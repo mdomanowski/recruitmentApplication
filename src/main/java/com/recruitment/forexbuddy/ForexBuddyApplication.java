6648b2cbfc644e09fbf14d8e532e4f089079fadc
Michał Domanowski <michal.domanowski@ppuc.pl>
2022-05-05 21:15:05 +0200

    initial commit

---
+++ src/main/java/com/recruitment/forexbuddy/ForexBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.forexbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recruitment/forexbuddy/ForexBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.forexbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
